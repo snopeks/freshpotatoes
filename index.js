8337c688c51a6f8e1640e883f8c47b00783f551c
snopeks <creativesnopeks@gmail.com>
2018-02-15 18:24:31 -0800

    finally got join table working, cleaned up styles and made arrow fns

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ Promise.resolve()
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 app.get('*', function(req, res) {
-  res.status(404).send({message: "404 page not found"});
+  res.status(404).send({message: '404 page not found'});
 })
 
 // Sequelize connection
 
-var sequelize = new Sequelize('mainDB', null, null, {
+const sequelize = new Sequelize('mainDB', null, null, {
     host: 'localhost',
-    dialect: "sqlite",
+    dialect: 'sqlite',
     storage: './db/database.db',
 });
 
 
-
-var Genre = sequelize.define('genre',
+//Define the models
+const Genre = sequelize.define('genre',
   {
     id: {type: Sequelize.INTEGER, primaryKey: true},
     name: {type: Sequelize.STRING, allowNull: false}
@@ -36,7 +36,7 @@ var Genre = sequelize.define('genre',
     timestamps: false
   }
 )
-var Film = sequelize.define('film',
+const Film = sequelize.define('film',
   {
     id: {type: Sequelize.INTEGER, primaryKey: true},
     title: {type: Sequelize.STRING, allowNull: false},
@@ -61,42 +61,42 @@ var Film = sequelize.define('film',
   }
 )
 
+
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
   //Set up response data
-  var filmResponse = {
+  let filmResponse = {
     recommendations: [],
     meta: {
       limit: 10,
       offset: 0
     }
   };
-  var parentData = {
-    releaseDate: [],
-    parentGenre: [],
-  }
+  let matchingFilms;
   Film.findById(req.params.id)
-    .then(function(film){
-      //get the film genre id and release date
-      //and put into parent data object
-      var filmObject = film.dataValues
-      // var parentReleaseDate = new Date(filmObject.release_date)
-      // console.log(parentReleaseDate)
-      // parentData.releaseDate.push(parentReleaseDate)
-      // parentData.parentGenre.push(filmObject.genre_id)
+    .then((film) => {
+      if(film === null){
+        res.status(422).send({message: "film doesn't exist"})
+      }
+      //get the film data
+      let filmObject = film.dataValues
       return filmObject
+
     })
-    .then(function(filmObject){
+    .then((filmObject) => {
       //select other films in the same genre released
-      //within 15 years before and after the parent range
-      Film.belongsTo(Genre, {foreignKey: 'genre_id'})
-      console.log("in the next promise",filmObject)
-      var releaseDate = new Date(filmObject.release_date)
-      console.log(releaseDate)
-      var maxRange = `${releaseDate.getFullYear() + 15}-${releaseDate.getMonth()}-${releaseDate.getDate()}`
-      var minRange = `${releaseDate.getFullYear() - 15}-${releaseDate.getMonth()}-${releaseDate.getDate()}`
-      console.log(maxRange, minRange)
-      Film.findAll({
+      //within 15 years before and after the parent film release date
+      Film.belongsTo(Genre, {foreignKey: 'genre_id'});
+      let releaseDate = new Date(filmObject.release_date);
+      let maxRange = `${releaseDate.getFullYear() + 15}-${releaseDate.getMonth()}-${releaseDate.getDate()}`;
+      let minRange = `${releaseDate.getFullYear() - 15}-${releaseDate.getMonth()}-${releaseDate.getDate()}`;
+      return Film.findAll({
+        include: [
+          {
+            model: Genre
+          }
+        ],
+        attributes: ['id', 'title', ['release_date', 'releaseDate']],
         where: {
           genre_id: filmObject.genre_id,
           release_date: {
@@ -104,58 +104,69 @@ function getFilmRecommendations(req, res) {
           }
         }
       })
-      .then(function(filteredFilms){
-        console.log("in the result call")
-        console.log(filteredFilms[0])
-
-      })
-
-
     })
+    .then((relatedFilms) => {
+      let filmIds = [];
+      matchingFilms = relatedFilms.map(function(i){
+        return i.dataValues;
+      })
+      relatedFilms.forEach((film) => {
+        filmIds.push(film.id);
+      })
+      return filmIds;
+    })
+    .then((filmIds) => {
+      request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${filmIds}`, function(err, response, body){
+        //sort out avg ratings and reviews from body
+        let allMovieReviews = JSON.parse(response.body);
+        allMovieReviews.forEach((movie) => {
+          //look at each movie and find the ones with at least 5 reviews
+          if(movie.reviews.length >= 5){
+            let avgRating = 0;
+            movie.reviews.forEach((review, i) => {
+              //start calculating avg rating by summing the review ratings
+              avgRating += review.rating;
 
-    console.log(parentData)
-
-
-
-
-  //Get the base film by the req params
-  // var getFilmById = function(id){
-  //   console.log("in function")
-  //   sequelize.query(`SELECT * FROM films WHERE id = ${id}`, {type: sequelize.QueryTypes.SELECT})
-  //     .then(function(film){
-  //       if(film.length === 1){
-  //         filmResponse.films.push(film[0])
-  //
-  //         getSimilarFilms(film)
-  //       } else {
-  //         console.log("can't find this film")
-  //         res.status(404).send({message: "can't find this film"})
-  //       }
-  //     })
-  // }
-  // getFilmById(req.params.id)
-
-  //get films related by genre and release date
-  // var getSimilarFilms = function(film){
-  //   var parentReleaseDate = new Date(film[0].release_date)
-  //   console.log(parentReleaseDate)
-  //   sendResponse()
-  // }
+              if(i === movie.reviews.length -1){
+                //once we've added all the ratings, average them out to two decimals!
+                avgRating = Number(Math.round((avgRating/movie.reviews.length)+'e2')+'e-2');
+                if(avgRating >= 4.0){
+                  //if the avg rating for a film is equal to or greater than 4,
+                  // add the # of reviews and avg rating values to the correct matching film data
+                  matchingFilms.find((film, i) => {
+                    if(film.id === movie.film_id){
+                        //add the rating, # of reviews, and update genre field on this movie
+                        matchingFilms[i].averageRating = avgRating;
+                        matchingFilms[i].reviews = movie.reviews.length;
+                        matchingFilms[i].genre = matchingFilms[i].genre.name;
+                        //add the fully updated film to our recommendations array
+                        filmResponse.recommendations.push(matchingFilms[i]);
+                        return true
+                    }
+                  })
+                  console.log('average is at least 4!')
+                  
+                } else {
+                  console.log('average is not good enough')
+                }
+              }
+            })
+          }
+        }) //END OF allMovieReviews.forEach
+        //ready to send back the best recommendations!
+        sendRecommendations()
+      })
+    }) //END OF request to get 3rd party reviews
 
   // send successful response
-  var sendResponse = function(){res.status(200).json({
-    recommendations: filmResponse.recommendations,
-    meta: filmResponse.meta
-    });
-  };
-
-
-  console.log("film id", req.params.id)
-  // sanitize req.params
+  let sendRecommendations = () => {res.status(200).json(filmResponse)};
 }
 // TODO: DONE db call#1: get the film that matches the id from the api request (e.g. Harry potter, genre: horror, releasedate: 2000)
-// TODO: db call#2: from db grab genres and release dates that match the request (e.g. grab horror films released 15 years before and after 2000)
-// TODO: return an array of films that fit this criteria
-// TODO: find films within this array with min 5 reviews and min 4 stars using 3rd party api on each
-// TODO: return filtered recommendations
+// TODO: DONE db call#2: from db grab genres and release dates that match the request (e.g. grab horror films released 15 years before and after 2000)
+// TODO: DONE find films within this array with min 5 reviews and min 4 stars using 3rd party api on each
+// TODO: DONE return filtered recommendations
+// TODO: DONE work on passing the tests and error handling
+  // TODO: enable limit and offset
+// TODO: DONE need to add genre name to response.
+
 module.exports = app;
